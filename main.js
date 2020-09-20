2dcf38fce3447860aabc312bf20ce129ede173ad
Minh DInh <minhthedinh@users.noreply.github.com>
2020-09-20 13:31:38 -0700

    added status

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ for(const file of commandFiles) {
 
 client.once('ready', () => {
     console.log('I am ready!');
+    bot.user.setActivity('Simping for OblivionGhoul');
 });
 
 client.on('message', message => {
