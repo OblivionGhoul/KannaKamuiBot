ac86431b55662fd61df00b90d2d5a6ea4718ad77
Minh DInh <minhthedinh@users.noreply.github.com>
2020-09-20 17:45:04 -0700

    deleted vote

--- main.js
+++ main.js
@@ -55,8 +55,6 @@ client.on('message', message => {
         client.commands.get('user-info').execute(message, args);
     } else if (command == 'josh') {
         client.commands.get('josh').execute(message, args);
-    } else if (command == 'vote') {
-        client.commands.get('vote').execute(message, args);
     }
 });
 
