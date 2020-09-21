a9c8402a7ea7cf6567a391440c52e974f608dac3
Minh DInh <minhthedinh@users.noreply.github.com>
2020-09-21 11:34:02 -0700

    info fix

--- commands/info.js
+++ commands/info.js
@@ -4,15 +4,17 @@ module.exports = {
     name: 'info',
     description: "displays user info",
     execute(message, args) {
-        let mentionedMember = message.mentions.first();
+        let mentionedMember = message.mentions.members.first();
         let mentionedUser = message.mentions.users.first();
 
-        const userEmbed = new DiscordAPIError.RichEmbed()
+        const userEmbed = new Discord.MessageEmbed()
         .setTitle(`User information for ${mentionedUser.username}`)
         .addField('Username: ', `${mentionedUser.username}`)
         .addField('User ID: ', `${mentionedUser.id}`)
         .addField('Account Created: ', `${mentionedUser.createdAt}`)
         .addField('Joined the server at: ', `${mentionedMember.joinAt}`)
         .addField('User status: ', `${mentionedUser.presence.status}`)
+
+        message.channel.send(userEmbed).catch(err => console.log(err));
     }
 }
