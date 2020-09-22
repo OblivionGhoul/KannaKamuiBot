6f991c134f47d6c062bb3896f6f2111224dd3b17
Minh DInh <minhthedinh@users.noreply.github.com>
2020-09-21 20:13:35 -0700

    hanna quote

--- commands/hanna.js
+++ commands/hanna.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'hanna',
     description: "hanna",
     execute(message, args) {
-        message.channel.send('i');
+        message.channel.send('A trebuchet is a mechanism that can launch a 90kg stone projectile over 300 meters POGGERS');
     }
 }
