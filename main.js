17fb7f254d3afbd70787fa62da49f53bcf9dd418
Minh DInh <minhthedinh@users.noreply.github.com>
2020-10-05 19:24:14 -0700

    updated command handler

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
 const prefix = '-';
-
 const fs = require('fs');
-
 client.commands = new Discord.Collection();
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for(const file of commandFiles) {
     const command = require(`./commands/${file}`);
-
     client.commands.set(command.name, command);
 }
+//bot status
 client.once('ready', async() => {
     try {
         let serverIn = client.guilds.size;
@@ -32,7 +27,7 @@ client.once('ready', async() => {
         console.log(error)
     }
 });
-
+//welcome message
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(channel => channel.name === '〚☠〛welcome-to-da-inn')
     if (!channel) return;
@@ -42,86 +37,29 @@ client.on('guildMemberAdd', member => {
     if(!autoRole) return;
     member.roles.add(autoRole.id);
 })
-
+//leave message 
 client.on('guildMemberRemove', member => {
     const channel = member.guild.channels.cache.find(channel => channel.name === '〚☠〛welcome-to-da-inn')
     if (!channel) return;
 
     channel.send(`Hope you enjoyed your stay, ${member}! :(`)
 })
-
+//command handler
 client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
-
     const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLocaleLowerCase();
-
-    if(command === 'weather') {
-        client.commands.get('weather').execute(message, args);
-    } else if (command == 'minh') {
-        client.commands.get('minh').execute(message, args);
-    } else if (command == 'kay') {
-        client.commands.get('kay').execute(message, args);
-    } else if (command == 'bryce') {
-        client.commands.get('bryce').execute(message, args);
-    } else if (command == 'ethan') {
-        client.commands.get('ethan').execute(message, args);
-    } else if (command == 'say') {
-        client.commands.get('say').execute(message, args);
-    } else if (command == 'user-info') {
-        client.commands.get('user-info').execute(message, args);
-    } else if (command == 'josh') {
-        client.commands.get('josh').execute(message, args);
-    } else if (command == 'hanna') {
-        client.commands.get('hanna').execute(message, args);
-    } else if (command == 'jake') {
-        client.commands.get('jake').execute(message, args);
-    } else if (command == 'info') {
-        client.commands.get('info').execute(message, args);
-    } else if (command == 'cal') {
-        client.commands.get('cal').execute(message, args);
-    } else if (command == 'vote') {
-        client.commands.get('vote').execute(message, args);
-    } else if (command == 'ban') {
-        client.commands.get('ban').execute(message, args);
-    } else if (command == 'kick') {
-        client.commands.get('kick').execute(message, args);
-    } else if (command == 'roast') {
-        client.commands.get('roast').execute(message, args);
-    } else if (command == 'meme') {
-        client.commands.get('meme').execute(message, args);
-    } else if (command == 'rules') {
-        client.commands.get('rules').execute(message, args);
-    } else if (command == 'commands') {
-        client.commands.get('commands').execute(message, args);
-    } else if (command == 'server') {
-        client.commands.get('server').execute(message, args);
-    } else if (command == 'insta') {
-        client.commands.get('insta').execute(message, args);
-    } else if (command == 'purge') {
-        client.commands.get('purge').execute(message, args);
-    } else if (command == 'poll') {
-        client.commands.get('poll').execute(message, args);
-    } else if (command == 'tempmute') {
-        client.commands.get('tempmute').execute(message, args);
-    } else if (command == 'ticket') {
-        client.commands.get('ticket').execute(message, args);
-    } else if (command == 'close') {
-        client.commands.get('close').execute(message, args);
-    } else if (command == 'fremont') {
-        client.commands.get('fremont').execute(message, args);
-    } else if (command == 'avatar') {
-        client.commands.get('avatar').execute(message, args);
-    } else if (command == 'ascii') {
-        client.commands.get('ascii').execute(message, args);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
     }
-});
-
-client.on('message', message => {
-    let args = message.content.substring(prefix.length).split(" ");
-    switch (args[0]) { 
+    //help command
+    let args1 = message.content.substring(prefix.length).split(" ");
+    switch (args1[0]) { 
         case 'help':
-            message.author.send('Do -commands to get the list of commands for this bot.');
+            message.author.send('Do -commands to get the list of commands for this bot. You can also do -ticket to get support in Minh\'s Inn.');
         break;
     }
 });
