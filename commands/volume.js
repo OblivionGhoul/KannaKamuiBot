3df66e53ac859d3272bfe5df49ce3d0164b8f532
Minh DInh <minhthedinh@users.noreply.github.com>
2020-10-10 10:15:05 -0700

    Volume Limiter

--- commands/volume.js
+++ commands/volume.js
@@ -7,16 +7,19 @@ module.exports = {
     aliases: ["v", "vol"],
     async execute(client, message, args) {
         const channel = message.member.voice.channel;
-        if (!channel) return message.channel.send("I'm sorry but you need to be in a voice channel to play music!");
+        if (!channel) return message.channel.send("You need to be in a vc to play music.");
         const serverQueue = message.client.queue.get(message.guild.id);
         if (!serverQueue) return message.channel.send("There is nothing playing in this server.");
         if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
         serverQueue.volume = args[0];
         serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-        if (serverQueue.volume > 5) return message.channel.send('The max volume is 5');
+        if (serverQueue.volume > 5) {
+            serverQueue.connection.dispatcher.setVolumeLogarithmic(1);
+            return message.channel.send('The max volume is 5');
+        }
         let xd = new MessageEmbed()
-            .setDescription(`I set the volume to: **${args[0]}**`)
-            .setTitle("Server Volume Manager")
+            .setDescription(`The volume is now: **${args[0]}**`)
+            .setTitle("Music Volume")
             .setColor("BLUE")
         return message.channel.send(xd);
     },
