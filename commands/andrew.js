8b6f8faa3a3491ae2fad12f707eb763cd6f67596
Minh DInh <minhthedinh@users.noreply.github.com>
2020-10-12 12:19:06 -0700

    Added Andrew's Quote

---
+++ commands/andrew.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'andrew',
+    description: "quote",
+    aliases: ['CyanideAlmonds'],
+    execute(client, message, args) {
+        message.channel.send('Poggers');
+    }
+  }
