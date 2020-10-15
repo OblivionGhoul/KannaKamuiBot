8108147074202257b4580fc0cef504b377e54ccb
Minh DInh <minhthedinh@users.noreply.github.com>
2020-10-14 21:36:35 -0700

    Added Nobel's quote

---
+++ commands/nobel.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'nobel',
+    description: "quote",
+    aliases: [''],
+    execute(client, message, args) {
+        message.channel.send('NOBEL BEAT ETHAN AT BVALS');
+    }
+  }
