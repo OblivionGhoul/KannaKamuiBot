df901045ea536874861de2a631ae74deeac9cf14
Minh DInh <minhthedinh@users.noreply.github.com>
2020-11-03 12:28:45 -0800

    Bot and Server info commands

---
+++ commands/commands/utility/botinfo.js
@@ -0,0 +1,27 @@
+const { MessageEmbed, version: djsversion } = require('discord.js');
+const { version } = require('@root/package.json');
+const ms = require('ms');
+
+module.exports = {
+    commands: ['botinfo', 'bot-info'],
+    callback: async (message, args, user, client) => {
+        const embed = new MessageEmbed()
+            .setThumbnail(client.user.displayAvatarURL())
+            .setColor(message.guild.me.displayHexColor || 'BLUE')
+            .addField('General', [
+                `**❯ Client:** ${client.user.tag} (${client.user.id})`,
+                `**❯ Servers:** ${client.guilds.cache.size.toLocaleString()} `,
+                `**❯ Users:** ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`,
+                `**❯ Channels:** ${client.channels.cache.size.toLocaleString()}`,
+                `**❯ Creation Date:** ${client.user.createdAt}`,
+                `**❯ Node.js:** ${process.version}`,
+                `**❯ Version:** v${version}`,
+                `**❯ Discord.js:** v${djsversion}`,
+                '\u200b'
+            ])
+            .setFooter('Bot made by OblivionGhoul#5842')
+            .setTimestamp();
+
+        message.channel.send(embed);
+    },
+}
