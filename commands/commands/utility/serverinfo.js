657029cd9bfd9d348ba52e9baeaf9faaefd389a3
Minh DInh <minhthedinh@users.noreply.github.com>
2020-11-03 12:35:31 -0800

    Fixed Serverinfo Command

--- commands/commands/utility/serverinfo.js
+++ commands/commands/utility/serverinfo.js
@@ -73,7 +73,7 @@ module.exports = {
                 `**❯ Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
                 '\u200b'
             ])
-            .addField(`Roles [${roles.length - 1}]`, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None')
+            .addField(`Roles [${roles.length - 1}]`, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? client.utils.trimArray(roles) : 'None')
             .setFooter('Bot made by OblivionGhoul#5842')
             .setTimestamp();
         message.channel.send(embed);
