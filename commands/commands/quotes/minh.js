42259f4bd4126f0d48b23b11971ff327427f9432
Minh DInh <minhthedinh@users.noreply.github.com>
2020-12-02 20:42:32 -0800

    Added Compliment Generator

--- commands/commands/quotes/minh.js
+++ commands/commands/quotes/minh.js
@@ -1,6 +1,15 @@
+const Discord = require("discord.js")
+const axios = require('axios');
+
 module.exports = {
-    commands: ['minh'],
-    callback: async (message) => {
-      message.channel.send('Minh is so cute!')
-    },
+    commands: ['compliment'],
+    callback: async (message, args) => {
+        axios.get('https://complimentr.com/api')
+            .then(function (response) {
+                message.channel.send(`Minh, ${response.data.compliment}.`)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 }
