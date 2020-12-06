5001bd6163297f207b594857d11a55b6880a00d0
Minh DInh <minhthedinh@users.noreply.github.com>
2020-12-05 22:22:47 -0800

    Bot and Server Info Command Fix

--- commands/commands/utility/userinfo.js
+++ commands/commands/utility/userinfo.js
@@ -44,8 +44,7 @@ module.exports = {
             .addField('Member', [
                 `**❯ Highest Role:** ${member.roles.highest.id === message.guild.id ? 'None' : member.roles.highest.name}`,
                 `**❯ Server Join Date:** ${moment(member.joinedAt).format('LL LTS')}`,
-                `**❯ Nickname:** ${member.nickname}`,
-                `**❯ Roles [${roles.length}]:** ${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None'}`,
+                `**❯ Nickname:** ${member.nickname}`
                 `\u200b`
             ])
             .setFooter("Bot Made By OblivionGhoul#5842", "https://i.imgur.com/Ivtf7GP.png")
