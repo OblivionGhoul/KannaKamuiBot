7fb18e53893fc96e8fef366ad84a55792b3c65b8
Minh DInh <minhthedinh@users.noreply.github.com>
2020-12-09 13:50:29 -0800

    Updated Discord Embed, Fix Args Docs

--- commands/commands/utility/discord.js
+++ commands/commands/utility/discord.js
@@ -5,6 +5,7 @@ module.exports = {
     callback: async (message, args) => {
         const embed = new MessageEmbed()
         .setTitle('Minh\'s Inn')
+        .setAuthor(message.member.displayName, message.member.user.displayAvatarURL())
         .setURL('https://discord.com/invite/VNAQrkQ')
         .setDescription('Join the Discord support server!')
         .setFooter("Bot Made By OblivionGhoul#5842", "https://i.imgur.com/Ivtf7GP.png")
--- commands/commands/utility/docs.js
+++ commands/commands/utility/docs.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 module.exports = {
     commands: ['docs', 'discord.js', 'discordjs'],
     callback: async (message, args) => {
-        if (!args) return message.channnel.send('Please provide a search query after the command.')
+        if (!args[0]) return message.channnel.send('Please provide a search query after the command.')
         const uri = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
             args
         )}`
