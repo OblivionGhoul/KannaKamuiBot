66560061230cf58738a09f5624f59db76f7fbff7
Minh DInh <minhthedinh@users.noreply.github.com>
2020-12-17 22:07:14 -0800

    Added Poggers Meme

---
+++ commands/commands/fun/poggers.js
@@ -0,0 +1,24 @@
+module.exports = {
+    commands: ['pog', 'poggers'],
+    callback: async (message, args, err) => {
+        const gif = [
+            'https://tenor.com/boUmz.gif',
+            'https://tenor.com/bp8pS.gif',
+            'https://tenor.com/bqzIv.gif',
+            'https://tenor.com/boUmw.gif',
+            'https://tenor.com/bn56o.gif',
+            'https://tenor.com/bo5So.gif',
+            'https://tenor.com/bppgK.gif',
+            'https://tenor.com/bnTWv.gif',
+            'https://tenor.com/bo9uV.gif',
+            'https://tenor.com/bq1GW.gif',
+            'https://tenor.com/boXZj.gif',
+            'https://tenor.com/boUmr.gif',
+            'https://tenor.com/bnU3l.gif',
+            'https://tenor.com/bo5Sp.gif',
+            'https://tenor.com/bkmre.gif',
+        ]
+        let index = (Math.floor(Math.random() * Math.floor(gif.length)));
+        message.channel.send(gif[index])
+    }
+}
--- commands/commands/utility/commands.js
+++ commands/commands/utility/commands.js
@@ -58,6 +58,7 @@ module.exports = {
             .addField('Hug', 'Hugs someone (ex: -hug [@usertag])')
             .addField('Kanna', 'Sends a picture of Kanna Kamui (ex: -kanna)')
             .addField('Tic-Tac-Toe', 'Sends a Tic-Tac-Toe game (ex: -ttt [@usertag])')
+            .addField('Poggers', 'Sends a poggers meme (ex: -poggers) (This command might be nsfw)')
             .addField('ChangeMyMind', 'Creates ChangeMyMind meme (ex: -changemymind [text])')
             .addField('Sign', 'Creates a picture of Hifumi with a sign (ex: -sign [text])')
             .addField('PH', 'Creates a PH comment (ex: -ph [text])')
