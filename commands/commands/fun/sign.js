d8cd22191b3e842c060e1621fcf9c04a08c5d196
Minh DInh <minhthedinh@users.noreply.github.com>
2020-12-29 14:25:32 -0800

    Fixed Sign Command

--- commands/commands/fun/sign.js
+++ commands/commands/fun/sign.js
@@ -1,14 +1,23 @@
-const Random = require("srod-v2");
+const Fetch = require("node-fetch")
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     commands: ['sign'],
-    callback: async (message, args) => {
+    callback: async (message) => {
         if (!message.content.includes(" ")) {
             return message.channel.send("Please specify what you want to say.")
         }
         message.channel.send('Creating image...')
         let input = message.content.substring(message.content.indexOf(' ') + 1)
-        let BlushFact = await Random.BlushFact(`${input}`, "BLUE");
-        message.channel.send(BlushFact);
+
+        let res = await Fetch(`https://nekobot.xyz/api/imagegen?type=fact&text=${input}`);
+        let json = await res.json();
+
+        if (!json.message) throw new Error(`Something Went Wrong, Try Again Later!`);
+
+        const data = new MessageEmbed()
+        .setImage(json.message)
+        .setColor('RANDOM')
+        message.channel.send(data)
     }
 }
