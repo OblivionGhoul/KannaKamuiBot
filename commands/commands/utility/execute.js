0a93f782c2d09c7102383d5f76dafef38f448d0e
Minh DInh <minhthedinh@users.noreply.github.com>
2021-01-12 18:12:42 -0800

    Locked Execute Command

--- commands/commands/utility/execute.js
+++ commands/commands/utility/execute.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process')
 module.exports = {
     commands: ['execute'],
     callback: async (message, args) => {
-        if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You need Admin perms to use this command.')
+        if (message.author.id !== '298589600132366339') return message.channel.send('You cannot use this command.')
         exec(args.join(' '), (error, stdout) => {
             const response = stdout || error;
             message.channel.send(response, {split: true, code: true});
