db4ce4363b84148964c2c08e1e1f2890d78c025f
Minh DInh <minhthedinh@users.noreply.github.com>
2021-01-25 10:06:51 -0800

    Updated Packages

--- commands/commands/quotes/ethan.js
+++ commands/commands/quotes/ethan.js
@@ -1,6 +1,6 @@
 module.exports = {
     commands: ['ethan', 'ethab'],
     callback: async (message) => {
-      message.channel.send('Cheating on Minh :(')
+      message.channel.send('andy is dumb')
     },
 }
