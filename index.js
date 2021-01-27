696d75c518e819d8cef708ba6bc059afb877b50f
Minh DInh <minhthedinh@users.noreply.github.com>
2021-01-27 14:57:22 -0800

    Added Bot Welcome Message

--- index.js
+++ index.js
@@ -81,4 +81,19 @@ client.on('message', message => {
   if (content.includes("ur mom") || content.includes("your mom")) return message.react('<:wazowski:735907372920209521>')
 })
 
+client.on('guildCreate', (guild) => {
+  const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
+  const embed = new Discord.MessageEmbed()
+  .setTitle('Thanks For Adding Me!')
+  .setDescription('Invite Me To Your Server [Here](https://discord.com/api/oauth2/authorize?client_id=757066313406611477&permissions=473427062&scope=bot)\nPlease Consider Donating [Here](https://www.paypal.com/paypalme/kannabot) To Keep It Running\nAny Issues Or Suggestions? Join My Support Server [Here](https://discord.gg/QpMWndNpse)\nMy Default Prefix Is [-]')
+  .setColor('RANDOM')
+  .addField('Help', 'Use this Command get the full command list\n(usage: -help)')
+  .addField('SetPrefix', 'Use this Command to change the default prefix\n(usage: -setprefix [new prefix])')
+  .setFooter('Bot Made By OblivionGhoul#5842', 'https://i.imgur.com/Ivtf7GP.png')
+  .setThumbnail('https://i.imgur.com/Zmr7TLZ.png')
+  .setURL('https://github.com/OblivionGhoul/KannaKamuiBot')
+
+  channel.send(embed)
+})
+
 client.login(process.env.token)
