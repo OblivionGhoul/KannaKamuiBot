a33a4e7ec52b405f20938c5fbed19b784ba3e3cb
Minh Dinh <45471929+OblivionGhoul@users.noreply.github.com>
2021-03-25 08:54:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,38 +27,6 @@ client.on('ready', async () => {
   loadFeatures(client)
 })
 
-//Server stats channel IDs
-let stats = {
-  serverID: '705454198464053331', //can change channel ID 
-  member: "738642924346146817", //can change channel ID 
-  bots: "738642927366176905" //can change channel ID 
-}
-
-//welcome message
-client.on('guildMemberAdd', member => {
-  const autoRole = member.guild.roles.cache.get('705475155782008936'); //can change auto role ID
-  if (!autoRole) return;
-  member.roles.add(autoRole.id);
-  //update server count
-  if (member.guild.id !== stats.serverID) return;
-  client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
-  client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
-})
-//leave message 
-client.on('guildMemberRemove', member => {
-  //update server count
-  if (member.guild.id !== stats.serverID) return;
-  client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
-  client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
-})
-
-client.on('message', message => {
-  var { content } = message
-  content = content.toLowerCase()
-  if (message.author.id !== '396850772488355841') return
-  if (content.includes("ur mom") || content.includes("your mom")) return message.react('<:wazowski:735907372920209521>')
-})
-
 client.on('guildCreate', (guild) => {
   const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
   const embed = new Discord.MessageEmbed()
