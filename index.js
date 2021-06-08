d87de131d77d5e1f487bdaa7394e967a20014f16
Minh Dinh <mdinh49@gmail.com>
2021-06-08 15:42:30 -0700

    Bug Fixes
    - Fixed issue with music queue after the bot leaves a VC
    - Updated NPM packages
    - Updated welcome menu

--- index.js
+++ index.js
@@ -32,16 +32,22 @@ client.on('ready', async () => {
 client.on('guildCreate', (guild) => {
   const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
   const embed = new Discord.MessageEmbed()
-  .setTitle('Thanks For Adding Me!')
-  .setDescription('Invite Me To Your Server [Here](https://discord.com/api/oauth2/authorize?client_id=757066313406611477&permissions=473427062&scope=bot)\nPlease Consider Donating [Here](https://www.paypal.com/paypalme/kannabot) To Keep It Running\nAny Issues Or Suggestions? Join My Support Server [Here](https://discord.gg/QpMWndNpse)\nMy Default Prefix Is [-]')
-  .setColor('RANDOM')
-  .addField('Help', 'Use this Command get the full command list\n(usage: -help)')
-  .addField('SetPrefix', 'Use this Command to change the default prefix\n(usage: -setprefix [new prefix])')
-  .setFooter('Bot Made By OblivionGhoul#5842', 'https://i.imgur.com/Ivtf7GP.png')
-  .setThumbnail('https://i.imgur.com/Zmr7TLZ.png')
-  .setURL('https://github.com/OblivionGhoul/KannaKamuiBot')
+    .setTitle('Thanks For Adding Me!')
+    .setDescription('Invite Me To Your Server [Here](https://discord.com/api/oauth2/authorize?client_id=757066313406611477&permissions=473427062&scope=bot)\nPlease Consider Donating [Here](https://www.paypal.com/paypalme/kannabot) To Keep It Running\nJoin My Support Server [Here](https://discord.gg/QpMWndNpse)\nMy Default Prefix Is [-]')
+    .setColor('RANDOM')
+    .addField('Help', 'Use this Command get the full command list\n(usage: -help)')
+    .addField('SetPrefix', 'Use this Command to change the default prefix\n(usage: -setprefix [new prefix])')
+    .setFooter('Bot Made By OblivionGhoul#5842', 'https://i.imgur.com/Ivtf7GP.png')
+    .setThumbnail('https://i.imgur.com/Zmr7TLZ.png')
+    .setURL('https://github.com/OblivionGhoul/KannaKamuiBot')
 
   channel.send(embed)
 })
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+  if (oldState.channelID === null || typeof oldState.channelID == 'undefined') return;
+  if (newState.id !== client.user.id) return;
+  return queue.delete(oldState.guild.id);
+});
 
 client.login(process.env.token)
