a80dea778b10a9699b3a437cf68b115da347d9e3
Minh Dinh <mdinh49@gmail.com>
2021-07-08 11:54:19 -0700

    Updated NPM Packages
    - Also Added "DC" and "Disconnect" For Music Leave Command

--- commands/commands/music/leave.js
+++ commands/commands/music/leave.js
@@ -1,5 +1,5 @@
 module.exports = {
-    commands: ['leave', 'l'],
+    commands: ['leave', 'l', 'dc', 'disconnect'],
     callback: async (message, args) => {
         const voiceChannel = message.member.voice.channel;
         if(!voiceChannel) return message.channel.send("You need to be in a voice channel to stop the music!");
