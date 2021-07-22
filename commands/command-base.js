a6bb85529065a9704d40605a72076aeebbb56cc3
Minh Dinh <mdinh49@gmail.com>
2021-07-22 14:14:20 -0700

    Bug Fixes
    - Updated NPM Packages
    - Stopped Logging Commands

--- commands/command-base.js
+++ commands/command-base.js
@@ -69,8 +69,6 @@ module.exports = (client, commandOptions) => {
     commands = [commands]
   }
 
-  console.log(`Registering command "${commands[0]}"`)
-
   // Ensure the permissions are in an array and are all valid
   if (permissions.length) {
     if (typeof permissions === 'string') {
