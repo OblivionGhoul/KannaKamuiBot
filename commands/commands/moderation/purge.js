c9e66c4880c05f0c1c4ffeb92f37c7924bf69317
SmugTheKiler <68525883+SmugTheKiler@users.noreply.github.com>
2021-08-11 21:34:32 +0200

    Added bot permission checker

--- commands/commands/moderation/purge.js
+++ commands/commands/moderation/purge.js
@@ -8,6 +8,10 @@ module.exports = {
 
         if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('You need the manage messages permission for this command!');
 
+        if(!message.guild.me.hasPermission('MANAGE_MESSAGES')) {
+          message.channel.send('I need permissions to delete messages please.')
+        }
+        
         let deleteAmount;
 
         if (isNaN(args1[0]) || parseInt(args1[0]) <= 0) { return message.reply('Please use a number between 1-99.') }
