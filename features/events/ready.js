4d311888dfdddb3958a3ab092e0d5c2c6d9a10db
SmugTheKiler <68525883+SmugTheKiler@users.noreply.github.com>
2021-08-12 18:47:15 +0200

    Update ready.js

--- features/events/ready.js
+++ features/events/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
     try {
       console.log(`I am watching over ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users and ${client.guilds.cache.size.toLocaleString()} servers!`);
       function pickStatus() {
-        let status = ['over Kobayashi', 'over Tohru', 'over Ilulu', 'over Makoto Takiya', 'over Quetzalcoatl', 'over Elma', 'over Shouta Magatsuchi', 'over Fafnir'];
+        let status = ['over Kobayashi', 'over Tohru', 'over Ilulu', 'over Makoto Takiya', 'over Quetzalcoatl', 'over Elma', 'over Shouta Magatsuchi', 'over Fafnir', 'your server];
         let Status = Math.floor(Math.random() * status.length);
 
         client.user.setActivity(status[Status], {
