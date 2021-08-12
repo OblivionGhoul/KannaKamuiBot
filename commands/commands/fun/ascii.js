a37d283b036d3323dfe676d3a9022e2d1cef955f
SmugTheKiler <68525883+SmugTheKiler@users.noreply.github.com>
2021-08-12 19:55:28 +0200

    ,,, no

--- commands/commands/fun/ascii.js
+++ commands/commands/fun/ascii.js
@@ -8,8 +8,8 @@ module.exports = {
 
         msg = args.join(" ");
 
-        if(msg < 20) return message.channel.send('Please provide text under 20 charecters!')
-        
+        if(args > 20) return message.channel.send('Please provide text under 20 charecters!')
+
         figlet.text(msg, function (err, data) {
             if (err) {
                 console.log('Something went wrong');
