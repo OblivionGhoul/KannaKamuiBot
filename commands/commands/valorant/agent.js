0f7f33886443df2648bb70be0f49ccee284cd664
Fabrizo <74118796+Fabrizio1663@users.noreply.github.com>
2021-10-30 23:55:11 -0500

    Update agent.js

--- commands/commands/valorant/agent.js
+++ commands/commands/valorant/agent.js
@@ -1,43 +1,62 @@
-const valorant = require('valorant-fetch')
-const { MessageEmbed } = require('discord.js')
+const valorant = require("valorant-fetch");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    commands: ['valagent', 'agent', 'val-agent', 'valorant-agent', 'valorantagent'],
-    callback: async (message, args) => {
-        let agent = args[0]
-        agent = agent.toLowerCase()
-        if (agent === 'breach'
-            || agent === 'brimstone'
-            || agent === 'cypher'
-            || agent === 'jett'
-            || agent === 'killjoy'
-            || agent === 'omen'
-            || agent === 'phoenix'
-            || agent === 'raze'
-            || agent === 'reyna'
-            || agent === 'sage'
-            || agent === 'sova'
-            || agent === 'skye'
-            || agent === 'viper'
-            || agent === 'yoru') {
-            let data = await valorant.getAgent(agent)
+  commands: [
+    "valagent",
+    "agent",
+    "val-agent",
+    "valorant-agent",
+    "valorantagent",
+  ],
+  callback: async (message, args) => {
+    let agent = args[0];
+    agent = agent.toLowerCase();
+    if (
+      agent === "breach" ||
+      agent === "brimstone" ||
+      agent === "cypher" ||
+      agent === "jett" ||
+      agent === "killjoy" ||
+      agent === "omen" ||
+      agent === "phoenix" ||
+      agent === "raze" ||
+      agent === "reyna" ||
+      agent === "sage" ||
+      agent === "sova" ||
+      agent === "skye" ||
+      agent === "viper" ||
+      agent === "yoru"
+    ) {
+      let data = await valorant.getAgent(agent);
 
-            const embed = new MessageEmbed()
-                .setAuthor(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
-                .setTitle(`${data.name} Agent Info`)
-                .setThumbnail(data.photos.icon)
-                .setDescription(`
-                **Type:** ${data.tags}\n
-                **Story:** ${data.biography.story}\n
-                **About:** ${data.biography.agent_about}\n
-                **Origin:** ${data.biography.region}\n
-                `)
-                .addField('Abilities', `\`Q\`: ${data.stats.Q}\n\`E\`: ${data.stats.E}\n\`C\`: ${data.stats.C} - **Signature**\n\`X\`: ${data.stats.X} - **Ultimate**`)
+      const array_description = [
+        `**Type:** ${data.tags}`,
+        `**Story:** ${data.biography.story}`,
+        `**About:** ${data.biography.agent_about}`,
+        `**Origin:** ${data.biography.region}`,
+      ];
 
-            message.channel.send(embed)
-        } else {
-            return message.channel.send('Please enter a valid agent.')
-        }
+      const arrray_stats = [
+        `\`Q\`: ${data.stats.Q}`,
+        `\`E\`: ${data.stats.E}`,
+        `\`C\`: ${data.stats.C}  - **Signature**`,
+        `\`X\`: ${data.stats.X} - **Ultimate**`,
+      ];
 
-    },
-}
+      const embed = new MessageEmbed()
+        .setAuthor(
+          message.member.displayName,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .setTitle(`${data.name} Agent Info`)
+        .setThumbnail(data.photos.icon)
+        .setDescription(array_description.join("\n"))
+        .addField("Abilities", arrray_stats.join("\n"));
+
+      message.channel.send(embed);
+    } else {
+      return message.channel.send("Please enter a valid agent.");
+    }
+  },
+};
